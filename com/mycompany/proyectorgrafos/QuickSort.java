622d8be458a8134a59597c0f0ce5c2d4c453f012
MiguelA536 <2410307@alumnos.univo.edu.mx>
2025-09-06 13:12:18 -0600

    Todo unido

--- com/mycompany/proyectorgrafos/QuickSort.java
+++ com/mycompany/proyectorgrafos/QuickSort.java
@@ -1,7 +1,10 @@
+package com.mycompany.proyectorgrafos;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class QuickSortSimpleInput {
+    
     public static void quickSort(int[] arr, int low, int high) {
         if (low < high) {
             int pi = partition(arr, low, high);
@@ -16,13 +19,18 @@ public class QuickSortSimpleInput {
         for (int j = low; j < high; j++) {
             if (arr[j] <= pivot) {
                 i++;
-                int temp = arr[i]; arr[i] = arr[j]; arr[j] = temp;
+                int temp = arr[i]; 
+                arr[i] = arr[j]; 
+                arr[j] = temp;
             }
         }
-        int temp = arr[i + 1]; arr[i + 1] = arr[high]; arr[high] = temp;
+        int temp = arr[i + 1]; 
+        arr[i + 1] = arr[high]; 
+        arr[high] = temp;
         return i + 1;
     }
 
+    // Método auxiliar para validar enteros
     public static int readInt(Scanner sc, String prompt) {
         System.out.print(prompt);
         while (!sc.hasNextInt()) {
@@ -33,16 +41,18 @@ public class QuickSortSimpleInput {
         return sc.nextInt();
     }
 
-    public static void main(String[] args) {
+    // 🔹 Método ejecutar para integrarlo al menú
+    public void ejecutar() {
         Scanner sc = new Scanner(System.in);
         int n = readInt(sc, "¿Cuántos números quieres ordenar? ");
         int[] arr = new int[n];
+        
         for (int i = 0; i < n; i++) {
             arr[i] = readInt(sc, "Ingresa el número #" + (i + 1) + ": ");
         }
+        
         System.out.println("Antes: " + Arrays.toString(arr));
         quickSort(arr, 0, arr.length - 1);
         System.out.println("Después: " + Arrays.toString(arr));
-        sc.close();
     }
 }
